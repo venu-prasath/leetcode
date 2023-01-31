bd2d3b41b2ee4055032fd0917d26a7479fe06f84
VenuPrasathM <venuprasath133@gmail.com>
2023-01-31 16:28:33 -0500

    LC153 commit

--- src/com/venuprasath/binary_search/LC153.java
+++ src/com/venuprasath/binary_search/LC153.java
@@ -14,10 +14,10 @@ public class LC153 {
 
     public int findMin(int[] nums) {
         int left = 0;
-        int right = nums.length-1;
+        int right = nums.length - 1;
 
         while(left < right) {
-            int mid = left + (right-left)/2;
+            int mid = left + (right - left) /2;
             if(nums[mid] > nums[right]) left = mid + 1;
             else if(nums[mid] < nums[right]) right = mid;
             else return nums[right];
