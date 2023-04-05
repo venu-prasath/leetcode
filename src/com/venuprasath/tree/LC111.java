be9e4575b0a7d6fa344a9231812538c2713079a3
VenuPrasathM <venuprasath133@gmail.com>
2023-04-05 15:24:21 -0400

    LC111

---
+++ src/com/venuprasath/tree/LC111.java
@@ -0,0 +1,30 @@
+package com.venuprasath.tree;
+
+public class LC111 {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        if(root.left == null && root.right == null) return 1;
+        if(root.left == null) return 1 + minDepth(root.right);
+        if(root.right == null) return 1 + minDepth(root.left);
+
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+}
