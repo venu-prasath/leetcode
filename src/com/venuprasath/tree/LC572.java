a233c91774f2df4a7adb404ecb4d07b250240c12
venu-prasath <venuprasath133@gmail.com>
2023-07-24 05:09:13 -0400

    LC572

---
+++ src/com/venuprasath/tree/LC572.java
@@ -0,0 +1,35 @@
+package com.venuprasath.tree;
+
+public class LC572 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isSubTree(TreeNode root, TreeNode sub) {
+        if(sub == null) return true;
+        if(root == null) return false;
+
+        return isSameTree(root, sub) || isSubTree(root.left, sub) || isSubTree(root.right, sub);
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        if(p == null || q == null || p.val != q.val) return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
