af4c68d281b4cd1fba143d924fe54737661ab715
venu-prasath <venuprasth133@gmail.com>
2024-03-13 06:17:16 -0400

    Single number ii with 3 solutions

--- src/com/venuprasath/bit_manipulation/SingleNumber2.java
+++ src/com/venuprasath/bit_manipulation/SingleNumber2.java
@@ -8,17 +8,21 @@ public class SingleNumber2 {
 
     public static void main(String[] args) {
         int[] nums = { 5, 5, 5, 2, 4, 4, 4 };
-        int result = find2(nums);
+        int result = magicSolution(nums);
         println(result);
     }
 
+    /*
+    Time Complexity: O(N * 32)
+    Space Complexity: O(1)
+     */
     public static int find(int[] nums) {
         int ans = 0;
 
         for(int i=0; i<32; i++) {
             int count = 0;
             for(int num: nums) {
-                count += num >> i & 1;
+                count += (num >> i) & 1;
             }
             count %= 3;
             ans |= count << i;
@@ -27,6 +31,10 @@ public class SingleNumber2 {
         return ans;
     }
 
+    /*
+    Time Complexity: O(Nlog(N) + N/3)
+    Space Complexity: O(1)
+     */
     public static int find2(int[] nums) {
         Arrays.sort(nums);
         for(int i = 1; i<nums.length; i += 3) {
@@ -35,5 +43,16 @@ public class SingleNumber2 {
             }
         }
         return nums[nums.length-1];
+    }
+
+    public static int magicSolution(int[] nums) {
+        int ones = 0;
+        int twos = 0;
+
+        for(int num: nums) {
+            ones = ones ^ (num & ~twos);
+            twos = twos ^ (num & ~ones);
+        }
+        return ones;
     }
 }
